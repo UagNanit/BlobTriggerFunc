1c120c206b7f57d4fb8707ec1926a5a5e6c6a90e
UagNanit <Olegkrava7@gmail.com>
2023-03-20 20:06:00 +0200

    cm

--- FunctionBLOBtrigger/Program.cs
+++ FunctionBLOBtrigger/Program.cs
@@ -2,15 +2,11 @@ using FunctionBLOBtrigger.Data;
 using FunctionBLOBtrigger.Repository;
 using FunctionBLOBtrigger.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Azure;
 
-internal class Program
+public class Program
 {
     private static string connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
     private static void Main(string[] args)
